f2c2884df92dd3e28fbbc188c8a8e7a6592e930e
PotoVic <wiktor.okonski39@gmail.com>
2025-04-24 12:53:50 +0200

    test

--- src/script/projects.js
+++ src/script/projects.js
@@ -1,7 +1,7 @@
  export const projectList = [{
     id:1,
     name:'Disney Quiz',
-    image:'src/media/disney-img.jpg',
+    image:'images/disney-img.jpg',
     alt:'Disney Quiz Website ',
     width:'1920',
     height:'915',
@@ -12,7 +12,7 @@
 {
     id:2,
     name:'Dough Almighty',
-    image:'src/media/dough-almighty-img.jpg',
+    image:'images/dough-almighty-img.jpg',
     alt:'Dough Almighty Website',
     width:'1901',
     height:'914',
@@ -23,7 +23,7 @@
 {
     id:3,
     name:'Lumina',
-    image:'src/media/lumina-img.jpg',
+    image:'images/lumina-img.jpg',
     alt:'Lumina Website',
     width:'1903',
     height:'916',
@@ -34,7 +34,7 @@
 {
     id:4,
     name:'SRs Humorprogram',
-    image:'src/media/srhumorprogram-img.jpg',
+    image:'images/srhumorprogram-img.jpg',
     alt:'Humorprogram image',
     width:'1907',
     height:'913',
