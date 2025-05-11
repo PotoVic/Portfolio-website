dde9db287913d0a7f211e99cd7ac2577b5effd7d
PotoVic <wiktor.okonski39@gmail.com>
2025-05-11 12:48:53 +0200

    tablet Responsive Styling

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import "./style/style.scss"
 import "./script/animation"
 import {createProject, projectList} from "./script/projects"
+
+//turn on btn for menu
 const hamMenu = document.querySelector('.hambrg-icon');
 const offScreenMenu = document.querySelector('.offScreenMenu');
 hamMenu.addEventListener('click', () => {
   hamMenu.classList.toggle('active');
   offScreenMenu.classList.toggle('active');
 })
+
+//turn off btn for menu
 const turnOffBtn = document.querySelector('.offButton');
 const menuScreen = document.querySelector('.offScreenMenu');
 turnOffBtn.addEventListener('click', () => {
@@ -14,5 +18,13 @@ turnOffBtn.addEventListener('click', () => {
   menuScreen.classList.remove('active');
 })
 
+// hides menu whenever u press any menu link
+const menuLinks = document.querySelectorAll('.offScreenMenu a');
+menuLinks.forEach(link =>{
+  link.addEventListener('click', ()=>{
+    offScreenMenu.classList.remove('active');
+    hamMenu.classList.remove('active'); // resets state of burger menu,
+  })
+})
 
 createProject()
